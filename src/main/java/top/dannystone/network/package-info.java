bdc31d7f11f5e7ddb7042dc06483de65210f96e0
daniel <daniel.zhang@ipiaoniu.com>
2018-10-30 23:46:56 +0800

    init

---
+++ src/main/java/top/dannystone/network/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: daniel
+ * Date: 2018-10-30
+ * Time: 下午11:04
+ */
+package top.dannystone.network;
---
+++ src/main/java/top/dannystone/server/KafkaServerApplication.java
@@ -0,0 +1,14 @@
+package top.dannystone.server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: daniel
+ * Date: 2018-10-30
+ * Time: 下午11:02
+ */
+public class KafkaServerApplication {
+    public static void main(String[] args){
+
+    }
+}
